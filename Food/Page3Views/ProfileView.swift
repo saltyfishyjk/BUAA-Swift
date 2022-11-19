e4942c79297081fc7850730f646db41fa2cca9d1
YJK <yjk_official@foxmail.com>
2022-11-19 17:43:35 +0800

    美化个人界面

--- Food/Page3Views/ProfileView.swift
+++ Food/Page3Views/ProfileView.swift
@@ -20,25 +20,69 @@ struct ProfileView: View {
     
     var body: some View {
         VStack{
+            HStack {
+                Text("Hello," + userName)
+                    .bold()
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.trailing)
+                    .foregroundColor(.blue)
+                    .padding(.leading, 20)
+                    .padding(.top, -40)
+                Spacer()
+            }
+            
             ScrollView(.vertical, showsIndicators: false) {
-                Text("hello,\(userName)!")
-                    .zIndex(1.0)
-                    .opacity((self.hero1 || self.hero2) ? 0 : 1)
-                Text("点赞的文章")
-                    .zIndex(1.0)
-                    .opacity((self.hero1 || self.hero2) ? 0 : 1)
+                //Text("hello,\(userName)!")
+                //    .zIndex(1.0)
+                //    .opacity((self.hero1 || self.hero2) ? 0 : 1)
+                // Text("点赞的文章")
+                //    .zIndex(1.0)
+                    // .opacity((self.hero1 || self.hero2) ? 0 : 1)
+                HStack {
+                    Image(systemName: "heart.fill")
+                        .foregroundColor(.red)
+                        .font(.title)
+                        .padding(.leading, 20)
+                    Text("点赞")
+                        .bold()
+                        .font(.title)
+                        .foregroundColor(.red)
+                        .multilineTextAlignment(.trailing)
+                        //.padding(.leading, 20)
+                        .zIndex(1.0)
+                        .opacity((self.hero1 || self.hero2) ? 0 : 1)
+                    Spacer()
+                }
                 HArticleListView(data: self.userData.articles.filter { self.userData.prefers[$0.id].likedUserId.contains(self.userData.userId) } ){ i in
                     hero1.toggle()
                     
                 } .opacity((self.hero2) ? 0 : 1)
                     .zIndex(5.0)
                 
-               
-                Text("收藏的文章")
+               /*
+                Text("收藏")
                     .opacity((self.hero1 || self.hero2) ? 0 : 1)
                     .zIndex(1.0)
                     .padding(.top,80)
+                */
                 
+                HStack {
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                        .font(.title)
+                        .padding(.leading, 20)
+                        .padding(.top, 80)
+                    Text("收藏")
+                        .bold()
+                        .font(.title)
+                        .foregroundColor(.yellow)
+                        .multilineTextAlignment(.trailing)
+                        //.padding(.leading, 20)
+                        .padding(.top, 80)
+                        .zIndex(1.0)
+                        .opacity((self.hero1 || self.hero2) ? 0 : 1)
+                    Spacer()
+                }
                 HArticleListView2(data: self.userData.articles.filter { self.userData.prefers[$0.id].staredUserId.contains(self.userData.userId) } ) { i in
                     hero2.toggle()
                 } .opacity((self.hero1) ? 0 : 1)
